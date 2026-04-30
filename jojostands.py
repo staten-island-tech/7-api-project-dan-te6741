ba224677a08b4b54cfe69dec50c43fddbc4fbcb1
dan-te6741 <danieli96@nycstudents.net>
2026-04-30 10:16:15 -0400

    stuff

---
+++ jojostands.py
@@ -0,0 +1,18 @@
+import requests
+
+def getslime(slime):
+    response = requests.get(f"https://github.com/apriltaoyvr/slime-rancher-api/{slime.lower()}")
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+    
+    data = response.json()
+    return {
+        "name": data["name"],
+        "height": data["height"],
+        "weight": data["weight"],
+        "types": [t["type"]["name"] for t in data["types"]]
+    }
+
+pokemon = getPoke("Bulbasaur")
+print(pokemon)
