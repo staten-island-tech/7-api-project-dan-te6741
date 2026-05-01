fe9451ef37241bcce621a5f5f0232104bb24365b
dan-te6741 <danieli96@nycstudents.net>
2026-05-01 09:23:44 -0400

    slimesdotnwork

--- slimes.py
+++ slimes.py
@@ -9,10 +9,7 @@ def getslime(slime):
     data = response.json()
     return {
         "name": data["name"],
-        "height": data["height"],
-        "weight": data["weight"],
-        "types": [t["type"]["name"] for t in data["types"]]
     }
 
-pokemon = getPoke("Bulbasaur")
-print(pokemon)
+slimer = getslime("Pink Slime")
+print(slimer)
