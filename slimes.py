dedb666f23eeb995ba8d873ec2334f5db8a102d6
dan-te6741 <danieli96@nycstudents.net>
2026-05-01 09:55:08 -0400

    imcooked

--- slimes.py
+++ slimes.py
@@ -1,15 +1,15 @@
 import requests
 
 def getslime(slime):
-    response = requests.get(f"https://github.com/apriltaoyvr/slime-rancher-api/{slime.lower()}")
+    response = requests.get(f"https://slime-rancher.vercel.app/api/slime")
     if response.status_code != 200:
-        print("Error fetching data!")
         return None
-    
     data = response.json()
-    return {
-        "name": data["name"],
-    }
+    print(data)
+    slimedata = [data['results']]
+    print(slimedata['name'])
+    
 
-slimer = getslime("Pink Slime")
+slimer = getslime("pink")
 print(slimer)
+
